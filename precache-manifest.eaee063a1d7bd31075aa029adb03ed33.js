7d7d41d91a945417677ee6db2f6d103bf1f0fded
Egle Gustaite <eglegustaite@gmail.com>
2020-02-24 11:07:02 +0200

    Updates

---
+++ precache-manifest.eaee063a1d7bd31075aa029adb03ed33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04027f07c734c05de9a52286bba904e2",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "9595f1ed7b67b2adb7ce",
+    "url": "/form/static/css/main.3d088ed1.chunk.css"
+  },
+  {
+    "revision": "edf6309950a8775e83ac",
+    "url": "/form/static/js/2.55a998d9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/form/static/js/2.55a998d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9595f1ed7b67b2adb7ce",
+    "url": "/form/static/js/main.b50885ec.chunk.js"
+  },
+  {
+    "revision": "d642ba86d6a86d32e38c",
+    "url": "/form/static/js/runtime-main.871c8494.js"
+  },
+  {
+    "revision": "0b0d8edc37e766fd88d3ea97eb80f0ff",
+    "url": "/form/static/media/network.0b0d8edc.mp4"
+  }
+]);
