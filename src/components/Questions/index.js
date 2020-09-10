1cddd02aa4f742b7654ba4ea65b954806841dd8d
Zmagis <eglegustaite@gmail.com>
2020-09-10 13:44:52 +0300

    remove title

--- src/components/Questions/index.js
+++ src/components/Questions/index.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import Spinner from "../Spinner";
+import Spinner from '../Spinner';
 
-const Questions = props => {
+const Questions = (props) => {
   const [questions, setQuestions] = useState([]);
   const [laoding, setLoading] = useState(false);
 
@@ -13,20 +13,20 @@ const Questions = props => {
 
   useEffect(() => {
     axios
-      .get("https://form-8987a.firebaseio.com/submits.json")
-      .then(response => {
+      .get('https://form-8987a.firebaseio.com/submits.json')
+      .then((response) => {
         const questionsArray = [];
         for (let key in response.data) {
           questionsArray.unshift({
             question: response.data[key].question,
             nickname: response.data[key].nickname,
-            color: response.data[key].color
+            color: response.data[key].color,
           });
         }
 
         setQuestions(questionsArray);
       })
-      .catch(error => {
+      .catch((error) => {
         alert(error);
       });
   }, [questions]);
@@ -37,11 +37,11 @@ const Questions = props => {
           <div
             style={{
               color: question.color,
-              fontWeight: "bold",
-              borderBottom: "2px dotted rgba(75, 78, 85, 0.651)",
+              fontWeight: 'bold',
+              borderBottom: '2px dotted rgba(75, 78, 85, 0.651)',
               minHeight: 80,
-              textAlign: "center",
-              fontSize: 24
+              textAlign: 'center',
+              fontSize: 24,
             }}
           >
             {question.question}
@@ -49,8 +49,8 @@ const Questions = props => {
           <div
             style={{
               color: question.color,
-              textAlign: "right",
-              paddingTop: 15
+              textAlign: 'right',
+              paddingTop: 15,
             }}
           >
             {question.nickname}
@@ -60,8 +60,8 @@ const Questions = props => {
     </div>
   );
   return (
-    <div className="questions-container">
-      <h1>Deep/Random Questions</h1>
+    <div className="questions-container" style={{ marginTop: '40px' }}>
+      {/* <h1>Deep/Random Questions</h1> */}
       {questions.length === 0 ? <Spinner /> : grid}
     </div>
   );
